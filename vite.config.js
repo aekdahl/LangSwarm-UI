1d70070a0ee134421093cbe0728b387b3ed9b4c4
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 20:56:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets (important for Ngrok and non-root deployments)
-  build: {
-    outDir: 'dist', // Ensure output directory is correct
-    rollupOptions: {
-      input: './index.html', // Ensure Vite uses the correct HTML file as entry
-    },
-  },
+  //base: './', // Use relative paths for assets (important for Ngrok and non-root deployments)
+  //build: {
+    //outDir: 'dist', // Ensure output directory is correct
+    //rollupOptions: {
+    //  input: './index.html', // Ensure Vite uses the correct HTML file as entry
+    //},
+  //},
 });
