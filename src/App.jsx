56a0f9c371e05c3bc147e2a35d03a028ca6ac7cc
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 22:11:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ChatWorkspace from "./components/ChatWorkspace";
+import LogsPage from "./components/LogsPage";
 
 const App = () => {
   return (
-    <div>
-      <ChatWorkspace />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ChatWorkspace />} />
+        <Route path="/logs" element={<LogsPage />} />
+      </Routes>
+    </Router>
   );
 };
 
