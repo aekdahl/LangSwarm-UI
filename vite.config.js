967bd86a1f093659101c4b30dc2707586119c303
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 21:19:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure the root is the project directory
-  build: {
-    outDir: 'dist', // Output directory
-    rollupOptions: {
-      input: './index.html', // Use index.html as the entry point
-    },
-  },
 });
