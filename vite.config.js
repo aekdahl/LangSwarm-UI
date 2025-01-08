1363ca2145b608dd9509a1e8ffde1c419a2b0750
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 19:50:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: './public', // Specify the directory where index.html is located
   build: {
     outDir: "dist",
   },
