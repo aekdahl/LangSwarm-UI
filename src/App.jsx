c8fd0d7e45262a94dd6cecae3896534d1c5a765c
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 13:55:52 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatWorkspace from "./components/ChatWorkspace";
+
+const App = () => {
+  return (
+    <div>
+      <ChatWorkspace />
+    </div>
+  );
+};
+
+export default App;
