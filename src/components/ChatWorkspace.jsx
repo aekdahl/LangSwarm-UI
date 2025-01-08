fb9f816c04ede0722090662c3d0b3252ab7b2f5d
aekdahl <alexander.ekdahl@gmail.com>
2025-01-08 22:57:32 +0100

    Update ChatWorkspace.jsx

--- src/components/ChatWorkspace.jsx
+++ src/components/ChatWorkspace.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Ensure react-router-dom is installed
-import "./styles.css";
+import "../styles.css";
 
 const ChatWorkspace = () => {
   const [messages, setMessages] = useState([]);
