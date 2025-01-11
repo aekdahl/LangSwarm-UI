e202a17d0291b48230ade2edcdf83db9f8498b7d
aekdahl <alexander.ekdahl@gmail.com>
2025-01-11 13:56:33 +0100

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,30 +1,77 @@
 // Message.js
 import React from "react";
 import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialLight } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const MessageContainer = styled.div`
   padding: 10px;
   margin: 5px;
   border-radius: 10px;
-  background-color: #f1f1f1;
   max-width: 80%;
+  align-self: ${(props) => (props.isUser ? "flex-end" : "flex-start")};
+  background-color: ${(props) => (props.isUser ? "#d1e7dd" : "#f8d7da")};
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  word-wrap: break-word;
 `;
 
-const UserMessage = styled(MessageContainer)`
-  background-color: #d1e7dd;
-  align-self: flex-end;
-`;
+const MarkdownMessage = styled.div`
+  font-family: Arial, sans-serif;
+  font-size: 0.95rem;
+  line-height: 1.5;
 
-const BotMessage = styled(MessageContainer)`
-  background-color: #f8d7da;
-  align-self: flex-start;
+  h1, h2, h3 {
+    margin: 0.5em 0;
+  }
+
+  p {
+    margin: 0.5em 0;
+  }
+
+  code {
+    background-color: #f1f1f1;
+    padding: 2px 4px;
+    border-radius: 4px;
+    font-family: "Courier New", Courier, monospace;
+  }
+
+  pre {
+    background-color: #f1f1f1;
+    padding: 10px;
+    border-radius: 5px;
+    overflow-x: auto;
+    font-family: "Courier New", Courier, monospace;
+  }
 `;
 
 const Message = ({ text, isUser }) => {
-  return isUser ? (
-    <UserMessage>{text}</UserMessage>
-  ) : (
-    <BotMessage>{text}</BotMessage>
+  return (
+    <MessageContainer isUser={isUser}>
+      <MarkdownMessage>
+        <ReactMarkdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  style={materialLight}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code {...props}>{children}</code>
+              );
+            },
+          }}
+        >
+          {text}
+        </ReactMarkdown>
+      </MarkdownMessage>
+    </MessageContainer>
   );
 };
 
