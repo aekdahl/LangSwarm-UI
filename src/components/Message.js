e243a881b1bafa1a142a16b879033d58d372350f
aekdahl <alexander.ekdahl@gmail.com>
2025-01-11 14:45:28 +0100

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,13 +4,13 @@ import styled from "styled-components";
 // Styled components
 const MessageWrapper = styled.div`
   display: flex;
-  justify-content: ${({ isUser }) => (isUser ? "flex-end" : "flex-start")};
+  //justify-content: ${({ isUser }) => (isUser ? "flex-end" : "flex-start")};
   margin: 5px 0;
 `;
 
 const MessageBubble = styled.div`
-  background-color: ${({ isUser }) => (isUser ? "#d1e7dd" : "#f8d7da")};
-  color: ${({ isUser }) => (isUser ? "#000" : "#000")};
+  //background-color: ${({ isUser }) => (isUser ? "#d1e7dd" : "#f8d7da")};
+  //color: ${({ isUser }) => (isUser ? "#000" : "#000")};
   border-radius: 10px;
   padding: 10px;
   max-width: 50%;
@@ -27,7 +27,7 @@ const Messages = ({ messages }) => {
     <>
       {messages.map((msg, index) => (
         <MessageWrapper key={index} isUser={msg.role === "user"}>
-          <MessageBubble isUser={msg.role === "user"}>
+          <MessageBubble>
             {msg.content || "Message content is missing"} {/* Fallback if content is undefined */}
           </MessageBubble>
         </MessageWrapper>
