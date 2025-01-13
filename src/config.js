df299a0ae8b7ccbe501a9b7786613fb11c095aa0
aekdahl <alexander.ekdahl@gmail.com>
2025-01-13 10:39:57 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
-export default BACKEND_URL;
+//const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+//export default BACKEND_URL;
